d740e0bbce5a341e1a2507293b66ff3c34647b1b
RachelWilliamsDev <Rachel@stacktiger.co>
2024-10-31 13:27:20 +0000

    feat: active collab list in header

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { revalidatePath } from "next/cache";
+import { liveblocks } from "../liveblocks";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: "Untitled Document",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ["room:write"],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.error(`Error happened while creating a room: ${error}`);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("You don't have access to this document");
+    // }
+    console.log(userId);
+    return parseStringify(room);
+  } catch (error) {
+    console.error(`Error happened while fetching a room: ${error}`);
+  }
+};
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,28 @@
 declare type HeaderProps = {
   children: React.ReactNode;
+  className?: string;
+};
+
+declare type UserType = "creator" | "editor" | "viewer";
+
+declare type CreateDocumentParams = {
+  userId: string;
+  email: string;
+};
+
+declare type RoomAccesses = Record<string, AccessType>;
+
+declare type AddDocumentBtnProps = {
+  userId: string;
+  email: string;
+};
+
+declare type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+declare type CollaborativeRoomProps = {
+  roomId: string;
+  roomMetadata: RoomMetadata;
 };
