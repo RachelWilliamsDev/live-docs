0e571a40a284f3d8ac41903b5034308c56ec5c30
RachelWilliamsDev <Rachel@stacktiger.co>
2024-11-01 12:06:11 +0000

    feat: share modal

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 import Loader from "@/components/Loader";
-import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getClerkUsers, getDocumentUsers } from "@/lib/actions/user.actions";
+import { useUser } from "@clerk/nextjs";
 import {
   ClientSideSuspense,
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
@@ -14,6 +17,14 @@ const Provider = ({ children }: { children: React.ReactNode }) => {
         const users = await getClerkUsers({ userIds });
 
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          text,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress || "",
+        });
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
